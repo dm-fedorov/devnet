3b2d1b5e6ece769ff6a8c1b56b6efc7c93f8f1fe
Dmitriy Fedorov <dmitriy.fedoroff@gmail.com>
2020-07-31 19:36:00 +0300

    обновил блокнот для админов

---
+++ L02-admin/some_module.py
@@ -0,0 +1,14 @@
+class Foo:
+    """мой класс Foo"""
+    def __init__(self):
+        pass
+
+class Bar:
+    """мой класс Bar"""
+    def __init__(self):
+        pass
+
+class Bam:
+    """мой класс Bam"""
+    def __init__(self):
+        pass
